1a1dd2d70c3873414347d617e6eea8523fb14020
hanyu0000 <3634802280@qq.com>
2024-11-12 20:02:24 +0800

    7

--- chapter7/a/kernel/global.h
+++ chapter7/a/kernel/global.h
@@ -13,13 +13,13 @@
 #define TI_LDT 1
 
 //定义不同的内核用的段描述符选择子
-#define SELECTOR_K_CODE	   ((1 << 3) + (TI_GDT << 2) + RPL0)
-#define SELECTOR_K_DATA	   ((2 << 3) + (TI_GDT << 2) + RPL0)
+#define SELECTOR_K_CODE    ((1 << 3) + (TI_GDT << 2) + RPL0)
+#define SELECTOR_K_DATA    ((2 << 3) + (TI_GDT << 2) + RPL0)
 #define SELECTOR_K_STACK   SELECTOR_K_DATA 
-#define SELECTOR_K_GS	   ((3 << 3) + (TI_GDT << 2) + RPL0)
+#define SELECTOR_K_GS      ((3 << 3) + (TI_GDT << 2) + RPL0)
 
 ////定义模块化的中断门描述符attr字段，attr字段指的是中断门描述符高字第8到16bit
-#define IDT_DESC_P 1 
+#define IDT_DESC_P 1
 #define IDT_DESC_DPL0 0
 #define IDT_DESC_DPL3 3
 #define IDT_DESC_32_TYPE 0xE   // 32位的门
