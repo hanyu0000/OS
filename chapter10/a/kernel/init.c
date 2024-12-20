3807231656ee82ac6c8b0e0b3ecacaea71fea5d4
hanyu0000 <3634802280@qq.com>
2024-12-20 12:04:02 +0800

    10

--- chapter10/a/kernel/init.c
+++ chapter10/a/kernel/init.c
@@ -3,10 +3,16 @@
 #include "interrupt.h"
 #include "../device/timer.h"
 #include "memory.h"
+#include "thread.h"
+#include "console.h"
+
 //负责初始化所有模块
 void init_all() {
    put_str("init_all\n");
    idt_init();             //初始化中断
-   timer_init();
    mem_init();             //初始化内存管理系统
+   thread_init();          //初始化线程相关结构
+   timer_init();
+   console_init();
+   keyboard_init();  // 键盘初始化
 }
